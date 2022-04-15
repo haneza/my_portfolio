2db0b45f34045705e92808305de070f30353c619
Henrietta Zarvou <henriettazarvou@gmail.com>
2022-04-15 16:40:59 +0300

    initilaize commit

---
+++ backend_sanity/schemas/skills.js
@@ -0,0 +1,25 @@
+export default {
+  name: "skills",
+  title: "Skills",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "bgColor",
+      title: "BgColor",
+      type: "string",
+    },
+    {
+      name: "imgUrl",
+      title: "ImgUrl",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
